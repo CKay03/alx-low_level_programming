c6769f22611042c327d259352b13de64cdc4b351
CKay03 <fiati.charles@gmail.com>
2023-10-05 18:56:17 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -o puzzle puzzle.c && ./puzzle
+echo '#include <stdio.h> int main() { puts("Programming is like building a multilingual puzzle"); return 0; }' > puzzle.c && gcc -o puzzle puzzle.c && ./puzzle
