98e61fa90f3eaa55826c33737dd5c3c1191fd9e4
CKay03 <fiati.charles@gmail.com>
2023-10-07 01:27:26 -0700

    variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always: 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar('0' + number);
+		number++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
