2f6986fe839958073b6822be98430efbfea742f5
CKay03 <fiati.charles@gmail.com>
2023-10-09 03:01:18 -0700

    variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always: 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num < 8; num1++)
+	{
+	for (num2 = num1 + 1; num2 < 9; num2++)
+	{
+	for (num3 = num2 + 1; num3 < 10; num 2++)
+	{
+	putchar((num1 % 10) + '0');
+	putchar((num2 % 10) + '0');
+	putchar((num3 % 10) + '0');
+
+	if (num1 == 7 && num2 == 8 && num3 == 9)
+	continue;
+	putchar(',');
+	Putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
