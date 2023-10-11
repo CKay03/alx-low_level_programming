bf637eb4954297bc2e678f441090c3c9d49dcaa9
CKay03 <fiati.charles@gmail.com>
2023-10-10 18:15:09 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	_putcchar('\n');
+
+	Return(0);
+}
