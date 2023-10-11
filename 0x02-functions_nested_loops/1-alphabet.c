480373014ef18a00c05becd6e178bbe46bc4fbe9
CKay03 <fiati.charles@gmail.com>
2023-10-10 19:44:50 -0700

    isalpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Make the alphabet
+ * print_alphabet - Make the alphabet c
  *
  * Return: void
  */
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *  _isalpha - Checks for alphabetic character
- *  c: character to be checked 
- *  Return: 1 for alphabetic character or 0 otherwise
+ * _isalpha - Checks for alphabetic character
+ * @c: character to be checked
+ * Return: 1 for alphabetic character or 0 otherwise
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 65 && c < 98) || (c >= 97 && c <= 122))
