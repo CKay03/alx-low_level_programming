4b35d50ef91db8c2e564f876328fe3d6873db524
CKay03 <fiati.charles@gmail.com>
2023-10-11 09:59:22 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int i, x = 0;
+	
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	x += i;
+	}
+	i++;
+	}
+	printf("%d\n", x);
+	return (0);
