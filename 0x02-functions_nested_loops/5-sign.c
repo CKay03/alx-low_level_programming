0c1c7d270650bbbf81d0b32e9e53c913199656a1
CKay03 <fiati.charles@gmail.com>
2023-10-11 10:20:30 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,28 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @n: the number to be checked
- * Return 1 for positive number, -1 for negative number or 0 for anything else
+ * @n: The number to be checked
+ * Return 1 for positive num, -1 for negative num or zero for anything else
  */
 
-int print_sign(int n);
+int print_sign(int n)
 
 {
-	if (n > 0) 
+	if (n > 0)
 	{
-	_putchar('+');
+	_putchar(43);
 	return (1);
-	} 
-	else if (n == 0) 
+	}
+	else if (n < 0)
 	{
-	_putchar('0');
-	return (0);
-	} 
-	else 
-	{
-	_putchar('-')
+	_putchar(45);
 	return (-1);
 	}
+	else
+	{
+	_putchar(48);
+
+	return (0);
+	}
 }
+
