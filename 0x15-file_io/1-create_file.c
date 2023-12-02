bcd5d2674982f982dbf19c285921416d0d67b7c2
CKay03 <fiati.charles@gmail.com>
2023-12-01 17:56:51 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	for (letters = 0; text_content[letters]; letters++)
 	;
 
-	rwrite = write(fd, text_content, nletters);
+	rwrite = write(gh, text_content, letters);
 
 	if (rwrite == -1)
 	return (-1);
